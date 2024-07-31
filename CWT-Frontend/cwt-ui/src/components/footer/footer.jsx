59c3d9ef641bf5033c91b71019d4dcc3c4385905
39ansh <141595198+39ansh@users.noreply.github.com>
2024-07-30 18:54:47 -0600

    footer commit

---
+++ CWT-Frontend/cwt-ui/src/components/footer/footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './footer.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="footer-logo">
+          <img src="Logo1.png" alt="Logo" />
+          <p>A large selection of quality products at a loyal price. Consult your manager if you need help with your selection. Using this site, you agree to the privacy policy and the terms of the public offer.</p>
+        </div>
+        <div className="footer-contacts">
+          <h3>CONTACTS</h3>
+          <p>2099 Walnut, Hill Ln undefined Lafayette, California 55262</p>
+          <p>+1 (256) 10 87 000</p>
+          <p>Mon - Sat: 10am - 6pm</p>
+          <p>Sunday: 10am - 4pm</p>
+        </div>
+        <div className="footer-services">
+          <h3>SERVICES</h3>
+          <ul>
+            <li>About Company</li>
+            <li>Payment</li>
+            <li>Delivery</li>
+            <li>Terms & Conditions</li>
+            <li>Privacy</li>
+          </ul>
+        </div>
+        <div className="footer-socials">
+          <h3>SOCIALS</h3>
+          <div className="social-icons">
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-facebook-f"></i>
+            <i className="fab fa-twitter"></i>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>© CTW®. All rights reserved.</p>
+        <div className="payment-methods">
+          <i className="fab fa-cc-paypal"></i>
+          <i className="fab fa-cc-visa"></i>
+          <i className="fab fa-cc-mastercard"></i>
+          <i className="fab fa-cc-amex"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- CWT-Frontend/cwt-ui/src/routes/Layout/layout.jsx
+++ CWT-Frontend/cwt-ui/src/routes/Layout/layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./layout.scss";
 import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/footer";
 
  function Layout() {
   return (
@@ -10,6 +11,9 @@ import Navbar from "../../components/navbar/Navbar";
     </div>
     <div className="content">
      <Outlet/>
+    </div>
+    <div className="footer">
+     <Footer />
     </div>
   </div>
   )
