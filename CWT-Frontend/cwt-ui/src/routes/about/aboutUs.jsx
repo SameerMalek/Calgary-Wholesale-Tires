23f830f06e8eddfb2ee0bd0ee6759bc11645ef7e
aryan-finix-amin <aryanfinixamin1104@gmail.com>
2024-10-16 13:52:07 -0600

    changes

--- CWT-Frontend/cwt-ui/src/routes/about/aboutUs.jsx
+++ CWT-Frontend/cwt-ui/src/routes/about/aboutUs.jsx
@@ -8,22 +8,24 @@ export default function AboutUs() {
         <div className="image">
           <img src="/assets/aboutus2.jpeg" alt="Calgary Wholesale Tyre" />
           <img src="/assets/aboutus4.jpg" alt="Calgary Wholesale Tyre" />
-        </div>
-        <div className="sub-heading">
-          <h2>YOUR WHOLESALE TIRE STORE IN CALGARY</h2>
-        </div>
+        </div>      
         <div className="info">
+        <h2 className='sub-heading'>YOUR WHOLESALE TIRE STORE IN CALGARY</h2>
           <p>
             We offer a diverse selection of new tires for cars, light trucks, SUVs, and trailers from top brands like Michelin, Bridgestone, Toyo, Cooper, Starfire, and Firestone, as well as Chinese tires like Yeada, Fortune, Linglong, and Firemax. Our selection includes winter tires, summer tires, all-season tires, high-performance tires, and drifting tires. "We have tires for every need and budget, making us the go-to destination for tires in Calgary." We specialize in tire installation in Calgary, and our state-of-the-art equipment ensures that your tires are installed correctly and safely. We are dedicated to providing our customers with the best service possible and strive to be a leading tire shop in Calgary.
           </p>
         </div>
-        <div className="sub-heading">
-          <h2>OUR MISSION</h2>
-        </div>
         <div className="info">
+          <h3>Our Mission</h3>
           <p>
-            Our mission is to provide the highest quality tires at an affordable price to the people of Calgary. We are committed to delivering exceptional customer service and ensuring that our clients leave our shop satisfied. We strive to be the best in the industry and to build long-lasting relationships with our customers.
+          At Calgary Wholesale Tires, our mission is to supply premium quality tires and accessories to wholesale clients at competitive prices. We are dedicated to providing seamless service and supporting our clients with expert advice and efficient order fulfillment. Whether it's bulk orders for new tires, accessories, or ongoing tire maintenance solutions, we aim to be the trusted partner for all tire-related needs. Our commitment to reliability, customer satisfaction, and industry leadership drives us to build strong, lasting relationships with our clients across Calgary and beyond.
           </p>
+          <h3>Our Values</h3>
+          <p>At Calgary Wholesale Tires, we are dedicated to providing our clients with exceptional service and competitive pricing. Honesty, integrity, and transparency are at the core of our business practices. We are committed to offering high-quality products and expert advice, empowering our clients to make well-informed decisions about their bulk tire and accessory needs. We take pride in our commitment to excellence and stand behind every product we sell. Our goal is to establish long-term relationships with our clients by consistently exceeding expectations. As a trusted and reliable wholesale tire supplier in Calgary and the surrounding regions, we always prioritize customer satisfaction and work diligently to ensure a smooth, hassle-free experience for every client.</p>
+          <h3>Solution</h3>
+          <p>At Calgary Wholesale Tires, we understand the importance of offering reliable and cost-effective tire solutions for a variety of vehicles. That's why we provide a vast selection of tires to meet the needs of our wholesale clients, whether for standard vehicles or high-performance models. Our knowledgeable team works closely with you to identify the best tire options for your specific requirements and budget. We take pride in delivering exceptional customer service and expert advice, ensuring that you are confident in every purchase. Whether you're sourcing new tires, arranging bulk orders, or need assistance with tire maintenance solutions, Calgary Wholesale Tires is your trusted partner for all your tire needs in Calgary and beyond.</p>
+          <h3>Local Tire Shop With a Wide Selection of Tires</h3>
+          <p>Welcome to Calgary Wholesale Tires, your trusted partner for high-quality tires at unbeatable wholesale prices. By importing our tires directly from global suppliers, we offer our clients the most competitive rates in Calgary without sacrificing quality. We believe that businesses deserve access to premium tire options at affordable prices, which is why we provide a wide selection to meet every need and budget. Our experienced team is here to assist you in finding the perfect tires for your inventory, with a variety of brands and sizes available. Thank you for choosing Calgary Wholesale Tires for all your bulk tire needs.</p>
         </div>  
       </section>
     </div>
